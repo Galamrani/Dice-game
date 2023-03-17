05aa1ca34ce582492974661698927645b95471e1
Gal Amrani <galamrani2@gmail.com>
2023-03-18 00:07:01 +0200

    add dice roll button to the game

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 
-var randomNum1 = Math.floor(Math.random() * 6) + 1;
-var randomNum2 = Math.floor(Math.random() * 6) + 1;
-var randomImage1 = "images/dice" + randomNum1 + ".png";
-var randomImage2 = "images/dice" + randomNum2 + ".png";
-
-var imgArray = document.querySelectorAll("img");
-
-imgArray[0].setAttribute("src", randomImage1);
-imgArray[1].setAttribute("src", randomImage2);
 
 
-if (randomNum1 > randomNum2)
-{
-    document.querySelector("h1").innerHTML = "!!Player 1 wins!!"
+document.querySelector(".btn").addEventListener("click", function () {
+
+    let randomNum1 = Math.floor(Math.random() * 6) + 1;
+    let randomNum2 = Math.floor(Math.random() * 6) + 1;
+
+    setRandomImg(randomNum1, randomNum2);
+    setWinnerHeader(randomNum1, randomNum2);
+
+});
+
+function setRandomImg(num1, num2)
+{ 
+    let randomImage1 = "images/dice" + num1 + ".png";
+    let randomImage2 = "images/dice" + num2 + ".png";
+
+    let imgArray = document.querySelectorAll("img");
+
+    imgArray[0].setAttribute("src", randomImage1);
+    imgArray[1].setAttribute("src", randomImage2);
 }
-else if (randomNum1 < randomNum2)
+
+function setWinnerHeader(num1, num2)
 {
-    document.querySelector("h1").innerHTML = "!!Player 2 wins!!"
-}
-else
-{
-    document.querySelector("h1").innerHTML = "!!Its a Draw!!"
+    if (num1 > num2)
+    {
+        document.querySelector("h1").innerHTML = "!!Player 1 wins!!"
+    }
+    else if (num1 < num2)
+    {
+        document.querySelector("h1").innerHTML = "!!Player 2 wins!!"
+    }
+    else
+    {
+        document.querySelector("h1").innerHTML = "!!Its a Draw!!"
+    }
 }
